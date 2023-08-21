e010b1c2c2804dbbb0a798937cca40c158e7a460
murali003 <muraliraina003@gmail.com>
2023-08-21 11:43:15 +0530

    Added environment files

---
+++ dashboard-ms/src/environments/environment.ts
@@ -0,0 +1,30 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  google_analytics_tracking_id: "UA-260554959-3",
+  apiURL: 'https://leap-intelligence.tibilprojects.com',
+  // apiURL: 'http://localhost:3008',
+  // flaskAPI :"http://127.0.0.1:5002/flask/",
+  flaskAPI :"https://leap-intelligence.tibilprojects.com/flask/",
+  stateCode: "NA",
+  numberFormat: {
+    reports: {
+      locale: 'en-IN',
+      format: 'short'
+    }
+  },
+  config: "VSK",
+
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
